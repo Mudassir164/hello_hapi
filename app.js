200652272eca4c54c24b6f6a41d2e36b6b7765c2
Mudassir Ahmed <52211617+Mudassir164@users.noreply.github.com>
2023-07-19 11:02:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
       })
     );
   } else {
-    res.end("<h1>Successfully started a server</h1>");
+    res.end("<h1>Successfully started a server hello world</h1>");
   }
 });
 
